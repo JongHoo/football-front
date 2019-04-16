89de9a4426cf4a60f50f5562d67f3af2f5c3bcb5
JongHoo <monkeis7@gmail.com>
2019-04-16 22:59:21 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+#app
+  b-navbar(
+  toggleable="lg"
+  type="dark"
+  variant="info"
+  )
+    b-navbar-brand(target="/") FootBall
+    b-navbar-toggle(target="nav-collapse")
+    b-collapse(id="nav-collapse" is-nav)
+      b-navbar-nav
+        b-nav-item Standings
+        b-nav-item Calendar
+      b-navbar-nav.ml-auto
+        b-nav-item-dropdown(:text="userNm" right)
+          b-dropdown-item Profile
+          b-dropdown-item Log out
+  router-view
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      userNm: 'User'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/containers/Dashboard.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+#dashboard
+  #slides
+    b-carousel(
+    controls
+    indicators
+    backgroud="#ababab"
+    slide
+    style="text-shadow: 1px 1px 2px #333;"
+    )
+      b-carousel-slide.slide1(
+      img-blank
+      caption="Fooball League Standings"
+      text="Check your favorite team's standings."
+      )
+      b-carousel-slide.slide2(
+      img-blank
+      caption="Fooball League Calendar"
+      text="Check your favorite team's calendar."
+      )
+
+</template>
+
+<script>
+export default {
+  name: 'Dashboard'
+}
+</script>
+
+<style lang="scss">
+.carousel {
+  img {
+    height: 300px !important;
+  }
+  .slide1 {
+    background-image: linear-gradient(to right top, #051937, #004d7a, #008793, #00bf72, #a8eb12);
+  }
+  .slide2 {
+    background-image: linear-gradient(to right top, #ff00bd, #ff318c, #ff6666, #ff944f, #ffba50);
+  }
+}
+</style>
