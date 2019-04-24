bdc9ac72d7a57fa981452dc1145e86e5303cf10c
JongHoo <monkeis7@gmail.com>
2019-04-25 00:32:16 +0900

    리그, 시즌 셀렉트 박스 추가. 조회조건 변경 시 해당 조건 쿼리 수행.

--- src/containers/Standings.vue
+++ src/containers/Standings.vue
@@ -3,12 +3,19 @@
   br
   b-container
     b-row(align-h="start")
+      b-col(md="6")
       b-col(md="3")
         b-row
           b-col.vertical-center(md="4")
             span League :
           b-col.vertical-center(md="6")
-            b-form-select(v-model="selected" :options="leagueList" @change="changeLeague()")
+            b-form-select(v-model="selectedLeague" :options="leagueList" @change="changeCondition")
+      b-col(md="3")
+        b-row
+          b-col.vertical-center(md="4")
+            span Season :
+          b-col.vertical-center(md="6")
+            b-form-select(v-model="selectedSeason" :options="seasonList" @change="changeCondition")
 </template>
 
 <script>
@@ -16,7 +23,8 @@ export default {
   name: 'Standings',
   data () {
     return {
-      selected: 'EPL',
+      selectedLeague: 'EPL',
+      selectedSeason: '18-19',
       leagueList: [
         {
           text: 'EPL',
@@ -34,12 +42,46 @@ export default {
           text: 'Eredivisie',
           value: 'EREDIVISIE'
         }
+      ],
+      seasonList: [
+        {
+          text: '16-17',
+          value: '16-17'
+        },
+        {
+          text: '17-18',
+          value: '17-18'
+        },
+        {
+          text: '18-19',
+          value: '18-19'
+        }
       ]
     }
   },
   methods: {
-    changeLeague () {
-      let url = 'http://soccer.sportsopendata.net/v1/leagues/premier-league/seasons/16-17/standings'
+    changeCondition () {
+      const option = this.selectedLeague
+      let url = null
+      switch (option) {
+        case 'EPL':
+          url = `http://soccer.sportsopendata.net/v1/leagues/premier-league/seasons/${this.selectedSeason}/standings`
+          break
+        case 'LALIGA':
+          url = `http://soccer.sportsopendata.net/v1/leagues/liga/seasons/${this.selectedSeason}/standings`
+          break
+        case 'SERIEA':
+          url = `http://soccer.sportsopendata.net/v1/leagues/serie-a/seasons/${this.selectedSeason}/standings`
+          break
+        case 'EREDIVISIE':
+          url = `http://soccer.sportsopendata.net/v1/leagues/eredivisie/seasons/${this.selectedSeason}/standings`
+          break
+        default:
+          url = ''
+      }
+      if (!url) {
+        return false
+      }
       this.$http.get(url)
         .then((res) => {
           if (res.data.data && res.data.data.statusCode === '200') {
