b71f901f42133b04918b0033fa35d35a0ac45266
JongHoo Hwang <monkeis7@gmail.com>
2019-08-05 14:35:34 +0900

    19-20 시즌 추가

--- src/containers/Standings.vue
+++ src/containers/Standings.vue
@@ -104,6 +104,10 @@ export default {
         {
           text: '18-19',
           value: '18-19'
+        },
+        {
+          text: '19-20',
+          value: '19-20'
         }
       ],
       c4Leagues: [
