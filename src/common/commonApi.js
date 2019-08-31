4c09cfa6a23c6894af34494659dda2a5bc4e9dc1
JongHoo <monkeis7@gmail.com>
2019-08-31 22:29:43 +0900

    백엔드 api 주소 변경

--- src/common/commonApi.js
+++ src/common/commonApi.js
@@ -4,33 +4,33 @@ import qs from 'querystring'
 const urls = {
   // for User
   getLeagues () {
-    return axios.get('https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/leagues')
+    return axios.get('https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/leagues')
   },
   getStandings (league, season) {
-    return axios.get(`https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/standings/${league}/${season}`)
+    return axios.get(`https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/standings/${league}/${season}`)
   },
   getTeams (league, season) {
-    return axios.get(`https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/teams/${league}/${season}`)
+    return axios.get(`https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/teams/${league}/${season}`)
   },
   getMatches (league, season, team) {
-    return axios.get(`https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/matches/${league}/${season}/${team}`)
+    return axios.get(`https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/matches/${league}/${season}/${team}`)
   },
   // for Admin
   updateTeams (league, season) {
-    return axios.post(`https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/teams/${league}/${season}`)
+    return axios.post(`https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/teams/${league}/${season}`)
   },
   updateStandings (league, season) {
     return axios.post(
-      'https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/standings',
+      'https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/standings',
       qs.stringify({league, season})
     )
   },
   updateMatches (league, season) {
-    return axios.post(`https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/matches/${league}/${season}`)
+    return axios.post(`https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/matches/${league}/${season}`)
   },
   login (loginId, loginPw) {
     return axios.post(
-      'https://3y4mhvmwq3.execute-api.ap-northeast-2.amazonaws.com/dev/login',
+      'https://uqy00myv2i.execute-api.ap-northeast-2.amazonaws.com/dev/login',
       qs.stringify({loginId, loginPw})
     )
   }
